5689d6b2190a0b68d2760411bc69a7606001bf40
harsh patel <hap56@afsconnect1.njit.edu>
2017-12-17 12:43:13 -0500

    chnages made in file

--- autoload.php
+++ autoload.php
@@ -1,17 +1,26 @@
 <?php
 class Manage
 {
+public static function config()
+{
+$data['site_name'] = 'Todo Task Management';
+return $data;
+}	
 public static function core($class)
 {
 $path = 'core/' . str_replace('\\', '/', $class) . '.php';
-if (is_file($path)) {
+//this is useful to see what path is being asked for
+if (is_file($path)) 
+{
 include $path;
 return;
 }
 }
 public static function controllers($class)
 {
+//use-what class and namespace are being asked for
 $path = 'controllers/' . str_replace('\\', '/', $class) . '.php';
+//use-what path is being asked for
 if (is_file($path)) {
 include $path;
 return;
@@ -20,7 +29,8 @@ return;
 public static function models($class)
 {
 $path = 'models/' . str_replace('\\', '/', $class) . '.php';
-if (is_file($path)) {
+if (is_file($path)) 
+{
 include $path;
 return;
 }
@@ -35,18 +45,17 @@ return;
 }
 public static function routes($class)
 {
-
 $path = 'routes/' . str_replace('\\', '/', $class) . '.php';
+ if (is_file($path)) 
+{
+ include $path;
+ return;
+}
+}
+}
 
-if (is_file($path)) {
-include $path;
-return;
-}
-}
-}
 spl_autoload_register(array('Manage', 'routes'));
 spl_autoload_register(array('Manage', 'controllers'));
 spl_autoload_register(array('Manage', 'collections'));
 spl_autoload_register(array('Manage', 'models'));
 spl_autoload_register(array('Manage', 'core'));
-?>
