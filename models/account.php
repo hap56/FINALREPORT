3b3cf22e1d3dfb06f1155e5c61f45d2b2f949a09
harsh patel <hap56@afsconnect1.njit.edu>
2017-12-17 13:27:39 -0500

    account and todo files added

---
+++ models/account.php
@@ -0,0 +1,46 @@
+<?php
+
+final class account extends \database\model
+{
+public $id;
+public $email;
+public $fname;
+public $lname;
+public $phone;
+public $birthday;
+public $gender;
+public $password;
+protected static $modelName = 'account';
+
+ public static function getTablename()
+  {
+   $tableName = 'accounts';
+   return $tableName;
+  }
+   public static function findTasks()
+  {
+  $records = todos::findAll();
+  print_r($records);
+  return $records;
+   }
+   
+  public function setPassword($password) 
+  {
+  $password = password_hash($password, PASSWORD_DEFAULT);
+  return $password;
+  }
+   public function checkPassword($LoginPassword) {
+   return password_verify($LoginPassword, $this->password);
+  }
+  public function validate()
+   {
+   $valid = TRUE;
+   echo 'myemail: ' . $this->email;
+   if($this->email == '') {
+   $valid = FALSE;
+   echo 'nothing in email';
+    }
+    return $valid;
+    }
+}
+?>
