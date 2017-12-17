106538ac364304dc01fc5eee009ab17c6465037c
harsh patel <hap56@afsconnect1.njit.edu>
2017-12-17 12:31:54 -0500

    changes in database file

--- database.php
+++ database.php
@@ -3,4 +3,33 @@ define('DATABASE', 'hap56');
 define('USERNAME', 'hap56');
 define('PASSWORD', 'dApqjS3S');
 define('CONNECTION', 'sql2.njit.edu');
-?>
+
+class dbConn
+{
+//connection object.
+protected static $db;
+//private construct class
+private function __construct() 
+{
+  try 
+   {
+ //assign PDO object to db variable
+              self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+              self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  }
+   catch (PDOException $e)
+  {
+  //error into error file logging
+   echo "Connection Error: " . $e->getMessage();
+   }
+ }
+ public static function getConnection() 
+{
+if (!self::$db) 
+{
+//new connection object.
+new dbConn();
+}
+  return self::$db;
+        }
+}
